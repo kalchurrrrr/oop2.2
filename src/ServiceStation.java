985406abc0ffedbf843c7c57cd155a3c30b88111
kalchurrrrr <nikolaykalchu@gmail.com>
2023-06-14 19:57:08 +0300

    Проверьте пожалуйста!

---
+++ src/ServiceStation.java
@@ -0,0 +1,14 @@
+public class ServiceStation {
+    public void check(Vehicle vehicle) {
+        System.out.println("Обслуживаем " + vehicle.getModelName());
+        for (int i = 0; i < vehicle.getWheelsCount(); i++) {
+            vehicle.updateTyre();
+        }
+        if (vehicle instanceof Car) {
+            ((Car) vehicle).checkEngine();
+        } else if (vehicle instanceof Truck) {
+            ((Truck) vehicle).checkEngine();
+            ((Truck) vehicle).checkTrailer();
+        }
+    }
+}
